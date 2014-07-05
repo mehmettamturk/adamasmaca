bb3c61973f623fba3243653c5e7e7b65d1b0046a
Mehmet Tamturk <mehmet.tamturk@startup.kitchen>
2014-07-06 01:36:59 +0300

    Add user view.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,38 +21,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/user/:username', function(req, res) {
+    var username = req.params.username;
+    res.send(username);
 });
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 
 module.exports = app;
--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,53 @@
 'use strict';
 
 /* App Module */
-
 angular.module('adamAsmaca', [ 'ngRoute' ]).config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/', {
             templateUrl: 'partials/main.html',
             controller: 'MainCtrl'
         }).
+        when('/user/:username', {
+            templateUrl: 'partials/user.html',
+            controller: 'UserCtrl'
+        }).
         otherwise({
             redirectTo: '/'
         });
 }]);
 
+
 angular.module('adamAsmaca').controller('MainCtrl', ['$scope', function($scope) {
     $scope.sth = true;
 }]);
 
+
+angular.module('adamAsmaca').controller('UserCtrl', ['$scope', '$route', '$http', 'UserService', function($scope, $route, $http, UserService) {
+    $scope.username = $route.current.params.username;
+
+    UserService.getUser($scope.username, function(data) {
+        $scope.userData = data;
+    });
+}]);
+
+
+angular.module('adamAsmaca').factory('UserService', function($http) {
+    var UserService = {};
+
+    UserService.getUser = function(username, callback) {
+        $http.get('/user/' + username).success(function(data) {
+            callback(data);
+        })
+    };
+
+    return UserService;
+});
+
+
+angular.module('adamAsmaca').filter('capitalize', function() {
+    return function(input, scope) {
+        if (input == null)
+            return '';
+        return input.substring(0,1).toUpperCase() + input.substring(1);
+    }
+});
