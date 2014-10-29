651e0bf7038ca25369f0289ba28274b497630e61
Mehmet Tamturk <mehmet.tamturk@startup.kitchen>
2014-10-29 15:15:09 +0200

    Add privacy.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Adam Asmaca' });
 });
 
+/* GET home page. */
+router.get('/privacy', function(req, res) {
+    res.render('privacy', { title: 'Adam Asmaca Gizlilik Sözleşmesi' });
+});
+
 module.exports = router;
